c0db4f6cdc46428eba58a2c68f857575643eca41
PedroLucas1601 <plcastro16@gmail.com>
2022-05-11 05:22:48 -0300

    first commit

---
+++ Dicionario.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_dicionario
+{
+    class Dicionario
+    {
+        string palavra { get; set; }
+
+        
+
+
+        public void PortuguesIngles(string palavra)
+        {
+            string linha = "",palavra1 = "";
+            bool ok = false;
+
+
+            StreamReader ler = new StreamReader("VerbosPortugues.txt");
+            
+
+
+            while (!ler.EndOfStream)
+            {
+                linha = ler.ReadLine();
+                string[] item = linha.Split(' ');
+                palavra1 = item[0];
+
+                if (palavra1.ToUpper() == palavra.ToUpper())
+                {
+                    Console.WriteLine($" Tradução: {item[1]}");
+                    ok = true;
+                    break;
+                }
+
+            }
+            ler.Close();
+            Console.WriteLine();
+            Console.WriteLine(ok ? "Palavra encontrada!" : "Palavra não encontrada... Digite novamente!");
+            Console.ReadKey();
+            
+
+
+        }
+
+        public void InglesPortugues(string palavra)
+        {
+            string linha = "", palavra1 = "";
+            bool ok = false;
+
+
+            StreamReader ler = new StreamReader("VerbosIngles.txt");
+
+
+
+            while (!ler.EndOfStream)
+            {
+                linha = ler.ReadLine();
+                string[] item = linha.Split(' ');
+                palavra1 = item[0];
+
+                if (palavra1.ToUpper() == palavra.ToUpper())
+                {
+                    Console.WriteLine($" Tradução: {item[1]} ");
+                    ok = true;
+                    break;
+                }
+
+            }
+            ler.Close();
+            Console.WriteLine();
+            Console.WriteLine(ok ? "Palavra encontrada!" : "Palavra não encontrada... Digite novamente!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace projeto_dicionario
+{
+    class Program
+    {
+        static void Main()
+        {
+            int op = 0;
+            string palavra = "";
+            bool ok = false;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("***** Dicionário de Verbos Irregulares *****");
+                Console.WriteLine("*****     Escolha uma das opções       *****");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(" 1. Procurar: português - inglês");
+                Console.WriteLine(" 2. Procurar: inglês - português");
+                Console.WriteLine(" 3. sair ");
+
+                
+                do
+                {
+                    op = int.Parse(Console.ReadLine());
+                } while (op < 0 || op > 3);
+
+                Dicionario dicionario = new Dicionario();
+
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Digite uma palavra em português para ser traduzida em inglês");
+                        palavra = Console.ReadLine();
+                        dicionario.PortuguesIngles(palavra);
+                        
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite uma palavra em inglês para ser traduzida para o português");
+                        palavra = Console.ReadLine();
+                        dicionario.InglesPortugues(palavra);
+                        break;
+                    case 3:
+                        break;
+                    
+                }
+
+
+
+
+
+
+
+
+
+            } while (op != 3);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
